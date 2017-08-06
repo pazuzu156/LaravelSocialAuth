819d1bb443c96548f0a3df336d5c084f794d4345
Kaleb Klein <klein.jae@gmail.com>
2017-08-06 19:39:57 -0400

    Force HTTPS
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- public/index.php
+++ public/index.php
@@ -7,6 +7,12 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+// Force HTTPS
+if (!isset($_SERVER['HTTPS']) && ($_SERVER['HTTP_HOST'] !== 'localhost')) {
+    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    exit;
+}
+
 define('LARAVEL_START', microtime(true));
 
 /*
